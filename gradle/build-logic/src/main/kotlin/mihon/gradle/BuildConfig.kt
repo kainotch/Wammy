a2b3e1f1cf9c845e6f7a3b56472c7493a1b71fb3
kainotch <krishnaxnotch@gmail.com>
2026-09-18 17:44:37 +0530

    Enable updater by default for all builds

--- gradle/build-logic/src/main/kotlin/mihon/gradle/BuildConfig.kt
+++ gradle/build-logic/src/main/kotlin/mihon/gradle/BuildConfig.kt
@@ -10,6 +10,7 @@ interface BuildConfig {
 
 val Project.Config: BuildConfig get() = object : BuildConfig {
     override val includeTelemetry: Boolean = project.hasProperty("include-telemetry")
-    override val enableUpdater: Boolean = project.hasProperty("enable-updater")
+    override val enableUpdater: Boolean = true
     override val includeDependencyInfo: Boolean = project.hasProperty("include-dependency-info")
 }
+
