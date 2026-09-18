989846d249edd5df273a88a63244ab09fd3f43eb
kainotch <krishnaxnotch@gmail.com>
2026-09-19 01:33:46 +0530

    Bump versionCode to 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
     defaultConfig {
         applicationId = "com.notch.wammy"
 
-        versionCode = 3
+        versionCode = 4
         versionName = "0.0.4"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getLatestCommitCount()}\"")
